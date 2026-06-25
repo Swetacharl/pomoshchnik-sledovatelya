853fc05089b69d08dccf6802293a36b7ac1f73db
Swetacharl <novertaeva@gmail.com>
2026-06-25 04:02:38 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,24 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkX5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5",
-  authDomain: "pomoshniksledovatelya.firebaseapp.com",
-  projectId: "pomoshniksledovatelya",
-  storageBucket: "pomoshniksledovatelya.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBw-oIUIU327OsJIVgtWCxjv-ibAJT4wqM",
+  authDomain: "forensichelper.firebaseapp.com",
+  databaseURL: "https://forensichelper-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "forensichelper",
+  storageBucket: "forensichelper.firebasestorage.app",
+  messagingSenderId: "875206695758",
+  appId: "1:875206695758:web:dc688de6e0bab2869adc1f"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Инициализация Firebase
 const app = initializeApp(firebaseConfig);
