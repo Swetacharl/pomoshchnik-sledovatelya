8170740788af9a59706eff3947fb0891fb67842c
Swetacharl <novertaeva@gmail.com>
2026-06-25 03:28:10 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,27 +1,26 @@
+// src/config/firebase.js
 import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Ваш конфиг из Firebase Console
 const firebaseConfig = {
-  apiKey: "AIzaSyBw-oIUIU327OsJIVgtWCxjv-ibAJT4wqM",
-  authDomain: "forensichelper.firebaseapp.com",
-  projectId: "forensichelper",
-  storageBucket: "forensichelper.firebasestorage.app",
-  messagingSenderId: "875206695758",
-  appId: "1:875206695758:web:dc688de6e0bab2869adc1f"
+  apiKey: "AIzaSyBkX5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5Z5",
+  authDomain: "pomoshniksledovatelya.firebaseapp.com",
+  projectId: "pomoshniksledovatelya",
+  storageBucket: "pomoshniksledovatelya.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
 };
 
-// Инициализация приложения
+// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
 
-// Настройка авторизации (с сохранением сессии)
-export const auth = initializeAuth(app, {
+// ✅ ИСПРАВЛЕНИЕ: Настройка persistence для сохранения сессии
+const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
 
-// База данных
-export const db = getFirestore(app);
+const db = getFirestore(app);
 
-export default app;
+export { auth, db };
